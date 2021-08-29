c1e76de356a26e8fe8d2dc11ac4c28002119a63a
Clint <clintoxen@gmail.com>
2021-08-29 16:04:28 -0400

    added some css to login

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,19 +3,19 @@ import React from 'react'
 export default function login() {
     return (
         <div className='grid h-screen w-screen content-center justify-center'>
-            <div className='login-wrapper tester'>
-                <h1 className=''>Please Log In</h1>
+            <div className='login-wrapper rounded globalmainbg-color tester opacity-75'>
+                <h1 className='my-2 text-lg text-header'>Log Into M-Dash</h1>
                 <form>
                     <label>
-                        <p>Username</p>
-                        <input type='text' />
+                        <p className='my-2 text-sub'>Username</p>
+                        <input className='input-bar text-gray-400' type='text' placeholder='Email' />
                     </label>
                     <label>
-                        <p>Password</p>
-                        <input type='password' />
+                        <p className='my-2 text-sub'>Password</p>
+                        <input className='input-bar text-gray-400' type='password' placeholder='Password' />
                     </label>
                     <div>
-                        <button type='submit'>Login</button>
+                        <button className='my-2 px-3 py-1 bg-gray-400 text-gray-200' type='submit'>Login</button>
                     </div>
                 </form>
             </div>
